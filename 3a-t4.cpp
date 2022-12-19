d0356f1a3ca73e8700f3465cbdc8fd6efb5aea80
daryatarashkevich <114498232+daryatarashkevich@users.noreply.github.com>
2022-12-19 12:18:45 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -3,16 +3,22 @@ using namespace std;
 
 int main()
 {
-int k, i = 2, sum_k = 0, number;
+int k, i = 1, sum_k = 0; double number;
 cout << " Enter the amount of integers to sum = "; cin >> k;
-  if (k==0)
-    cout<<" because the number of integers is zero, it is impossibleto calculate the sum "<<endl;
-  else{                                                                       
+
 do {
 cout << "Enter integer nr. " << i << ": "; 
 cin >> number;
+
+while (number-(int)number!=0)
+    {cout << "You should write integer. Try again" << endl; 
+cout << "Enter another number = "; 
+cin >> number;
+break;}
+
 sum_k += number; i++;
 } while (i <= k);
 cout << " The total sum of " << k << " integers is: "  << sum_k;
 return 0;
 }
+
